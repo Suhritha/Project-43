4f808b1a7f414b6775c1779416e7d14dcde1c9cd
Suhritha <72441456+Suhritha@users.noreply.github.com>
2021-06-25 19:42:54 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,31 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A E", 100,200)
+    fill("lightblue")
+    text("Hint: Collection of structured information , or data  !!", 100,210)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 100,250)
+    fill("lightblue")
+    text("Hint: Modules of code that accomplish a specific task !!", 100,270)
+
+
+// add code for displaying rest of the names and the hints.
+  
+
+    
+
+}
